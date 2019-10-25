37ee44efa2f02b90648aec1ac3a2afd12108391f
JhonPeterAguilarAtencio <xxjhonxxaguilarxx@gmail.com>
2019-10-25 01:07:52 -0500

    division:footer,head,js,sidenav,topbar
    Co-Authored-By: null <JhonPeterAguilarAtencio@users.noreply.github.com>

---
+++ Views/Template/head.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="utf-8" />
+    <title>Proyecto Taky - 2019</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A premium admin dashboard template by Mannatthemes" name="description" />
+    <meta content="Mannatthemes" name="author" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    <!-- App css -->
+    <link href="Assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="Assets/css/icons.css" rel="stylesheet" type="text/css" />
+    <link href="Assets/css/metisMenu.min.css" rel="stylesheet" type="text/css" />
+    <link href="Assets/css/style.css" rel="stylesheet" type="text/css" />
+
+</head>
---
+++ Views/Template/js.php
@@ -0,0 +1,16 @@
+<!-- jQuery  -->
+<script src="Assets/js/jquery.min.js"></script>
+<script src="Assets/js/bootstrap.bundle.min.js"></script>
+<script src="Assets/js/metisMenu.min.js"></script>
+<script src="Assets/js/waves.min.js"></script>
+<script src="Assets/js/jquery.slimscroll.min.js"></script>
+
+<!-- google maps api -->
+<script src="https://maps.google.com/maps/api/js?key=AIzaSyCtSAR45TFgZjOs4nBFFZnII-6mMHLfSYI"></script>
+<!-- Gmaps file -->
+<script src="Assets/plugins/gmaps/gmaps.min.js"></script>
+<!-- demo codes -->
+<script src="Assets/pages/jquery.gmaps.init.js"></script>
+
+<!-- App js -->
+<script src="Assets/js/app.js"></script>
