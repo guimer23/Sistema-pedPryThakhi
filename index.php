faa30de2fb3dc5b00fa2fd08a8269620c45708bc
JhonPeterAguilarAtencio <xxjhonxxaguilarxx@gmail.com>
2019-10-29 19:53:33 -0500

    Cambio en el Topbar, tiene que estar en el index, el topbar completo si no no lee las imagenes, no lo borren xd

--- index.php
+++ index.php
@@ -21,7 +21,85 @@
     <body>
 
         <!-- Top Bar Start -->
-        <?php include "Views/Template/topbar.php" ;?>
+        <div class="topbar">
+
+                    <!-- LOGO -->
+                    <div class="topbar-left">
+                        <a href="index.php" class="logo">
+                            <span>
+                                <img src="Assets/images/logo-sm.png" alt="logo-small" class="logo-sm">
+                            </span>
+                            <span>
+                                <img src="Assets/images/logo-dark.png" alt="logo-large" class="logo-lg">
+                            </span>
+                        </a>
+                    </div>
+                    <!--end logo-->
+                    <!-- Navbar -->
+                    <nav class="navbar-custom">
+                        <ul class="list-unstyled topbar-nav float-right mb-0">
+                            <li class="dropdown notification-list">
+                                <a class="nav-link dropdown-toggle arrow-none waves-light waves-effect" data-toggle="dropdown" href="#" role="button"
+                                    aria-haspopup="false" aria-expanded="false">
+                                    <i class="dripicons-bell noti-icon"></i>
+                                    <span class="badge badge-danger badge-pill noti-icon-badge">2</span>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right dropdown-lg">
+                                    <!-- item-->
+                                    <h6 class="dropdown-item-text">
+                                        Notificaciones (18)
+                                    </h6>
+                                    <div class="slimscroll notification-list">
+                                        <!-- item-->
+                                        <a href="javascript:void(0);" class="dropdown-item notify-item active">
+                                            <div class="notify-icon bg-success"><i class="mdi mdi-cart-outline"></i></div>
+                                            <p class="notify-details">La orden Nº 0023 fue entregado con éxito.<small class="text-muted">Dummy text of the printing and typesetting industry.</small></p>
+                                        </a>
+                                        <!-- item-->
+                                        <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                            <div class="notify-icon bg-warning"><i class="mdi mdi-message"></i></div>
+                                            <p class="notify-details">Nuevo mensaje recibido<small class="text-muted">You have 87 unread messages</small></p>
+                                        </a>
+                                    </div>
+                                    <!-- All-->
+                                    <a href="javascript:void(0);" class="dropdown-item text-center text-primary">
+                                        Ver todo <i class="fi-arrow-right"></i>
+                                    </a>
+                                </div>
+                            </li>
+
+                            <li class="dropdown">
+                                <a class="nav-link dropdown-toggle waves-effect waves-light nav-user" data-toggle="dropdown" href="#" role="button"
+                                    aria-haspopup="false" aria-expanded="false">
+                                    <img src="Assets/images/users/user-4.jpg" alt="profile-user" class="rounded-circle" />
+                                    <span class="ml-1 nav-user-name hidden-sm">Guimer <i class="mdi mdi-chevron-down"></i> </span>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <a class="dropdown-item" href="#"><i class="dripicons-user text-muted mr-2"></i> Perfil</a>
+                                    <a class="dropdown-item" href="#"><i class="dripicons-gear text-muted mr-2"></i> Configuraciones</a>
+                                    <a class="dropdown-item" href="#"><i class="dripicons-lock text-muted mr-2"></i> Bloquear pantalla</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#"><i class="dripicons-exit text-muted mr-2"></i> Cerrar sesión</a>
+                                </div>
+                            </li>
+                        </ul><!--end topbar-nav-->
+
+                        <ul class="list-unstyled topbar-nav mb-0">
+                            <li>
+                                <button class="button-menu-mobile nav-link waves-effect waves-light">
+                                    <i class="dripicons-menu nav-icon"></i>
+                                </button>
+                            </li>
+                            <li class="hide-phone app-search">
+                                <form role="search" class="">
+                                    <input type="text" placeholder="Buscar . . ." class="form-control">
+                                    <a href=""><i class="fas fa-search"></i></a>
+                                </form>
+                            </li>
+                        </ul>
+                    </nav>
+                    <!-- end navbar-->
+        </div>
         <!-- Top Bar End -->
 
         <div class="page-wrapper">
@@ -119,11 +197,11 @@
                               <h6 class="menu-title">Mantenimiento</h6>
                           </div>
                           <ul class="nav">
-                              <li class="nav-item"><a class="nav-link" href="../projects/projects-clients.html"><i class="mdi mdi-car-multiple"></i>Vehículos</a></li>
-                              <li class="nav-item"><a class="nav-link" href="Views/pedClsConductor/pedFrmListar.php"><i class="mdi mdi-google-street-view"></i>Conductores</a></li>
-                              <li class="nav-item"><a class="nav-link" href="../projects/projects-users.html"><i class="mdi mdi-train-car"></i>Vehículo - Conductor</a></li>
-                              <li class="nav-item"><a class="nav-link active" href="pedFrmListar.php"><i class="mdi mdi-account-multiple"></i>Clientes</a></li>
-                              <li class="nav-item"><a class="nav-link" href="../projects/projects-users.html"><i class="dripicons-user-group"></i>Usuarios</a></li>
+                            <li class="nav-item"><a class="nav-link" href="Views/pedClsVehiculo/pedFrmListar.php"><i class="mdi mdi-car-multiple"></i>Vehículos</a></li>
+                            <li class="nav-item"><a class="nav-link" href="Views/pedClsConductor/pedFrmListar.php"><i class="mdi mdi-google-street-view"></i>Conductores</a></li>
+                            <li class="nav-item"><a class="nav-link" href="Views/pedClsVehiculoConductor/pedFrmListar.php"><i class="mdi mdi-train-car"></i>Vehículo - Conductor</a></li>
+                            <li class="nav-item"><a class="nav-link" href="Views/pedClsCliente/pedFrmListar.php"><i class="mdi mdi-account-multiple"></i>Clientes</a></li>
+                            <li class="nav-item"><a class="nav-link" href="Views/pedClsUsuario/pedFrmListar.php"><i class="dripicons-user-group"></i>Usuarios</a></li>
                           </ul>
                       </div><!--Inicia SubMenu Mantenimiento -->
 
