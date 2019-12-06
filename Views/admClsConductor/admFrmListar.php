b862a53beee7f51cd919975a02b2e68a2e7f23c6
J-Alfaro <J-Alfaro>
2019-12-06 18:22:11 -0500

    d
    d

--- Views/admClsConductor/admFrmListar.php
+++ Views/admClsConductor/admFrmListar.php
@@ -296,8 +296,11 @@ console.log(r);
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
           $('#correov').text(dato['email']);
 =======
+=======
+>>>>>>> parent of b9f13f8... Cambio CONDUCTOR
 =======
 >>>>>>> parent of b9f13f8... Cambio CONDUCTOR
 =======
